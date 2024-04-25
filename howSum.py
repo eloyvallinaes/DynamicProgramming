eb9b220b730f10eb43b318e03bc478d6575d26c8
evallina <eloyvallina33@gmail.com>
2024-04-25 12:42:24 +0200

    howSum memoised and complexities.

--- howSum.py
+++ howSum.py
@@ -9,11 +9,16 @@ target  sum.
 """
 
 import time
+from canSum import timeit
 
 
-def recursive_howSum(t, arr, series=None):
+def recursive_howSum(t, arr):
     """
-    Recursive implementation. Time complexity O(t^n * t) where n is len(arr).
+    Recursive implementation.
+    Time complexity:
+        List extension operation: O(k), where len(list) = k. Worst case: k = t.
+        Recursive calls: O(m^t) where len(arr) = m.
+        Total: O(m^t * t)
 
     """
     if t == 0: return []
@@ -22,9 +27,32 @@ def recursive_howSum(t, arr, series=None):
     for v in arr:
         result = recursive_howSum(t-v, arr)
         if result != None:
-            return  result + [v]
+            return  result + [v]  # list extension has complexity O(k)
     
     return None
+
+
+def howSum(t, arr, memo=None):
+    """
+    Memoised implementation. O(m * t^2).
+
+    """
+    if memo == None:
+        memo = {}
+    if t in memo: return memo[t]
+    if t == 0: return []
+    if t < min(arr): return None
+
+    for v in arr:
+        result = howSum(t-v, arr, memo)
+        memo.update({
+            t-v: result
+        })
+        if result != None:
+            return  result + [v]  
+
+    return None
+
 
 
 if __name__ == '__main__':
@@ -32,3 +60,19 @@ if __name__ == '__main__':
     assert set(recursive_howSum(7, [4, 3])) == set([3, 4])
     assert recursive_howSum(7, [5, 3]) is None
     print("Recursive OK!")
+
+    assert howSum(7, [5, 3, 7]) == [7]
+    assert set(howSum(7, [4, 3])) == set([3, 4])
+    assert howSum(7, [5, 3]) is None
+    print("Memoised OK!")
+
+    # timings
+    target = 200
+    recursive_time = timeit(recursive_howSum, target, [7, 14])
+    memoised_time = timeit(howSum, target, [7, 14])
+    print(f"Recursive {recursive_time:.2f} ms")
+    print(f"Memoised {memoised_time:.2f} ms")
+
+
+
+
