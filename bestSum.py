0e2f0dfded51de21c9262c1961fcf91eada99c46
evallina <eloyvallina33@gmail.com>
2024-04-26 13:27:10 +0200

    bestSum: recursive

---
+++ bestSum.py
@@ -0,0 +1,40 @@
+"""
+Determine whether the shortest combination of numbers in arr that can be
+added to produce a target value.
+
+1. Array values may be used as many times as necessary.
+
+2. All input values are positive integers.
+
+3. Ties for shortest combination may return either.
+
+
+"""
+
+import math
+from canSum import timeit
+
+def recursive_bestSum(t, arr):
+    """
+    Recursive implementation. Time complexity
+    """
+    # end cases
+    if t == 0: return []
+    if t < min(arr): return None
+
+    best = None
+    for v in arr:
+        result = recursive_bestSum(t-v, arr)
+        if result is not None:
+            if  best is None or len(result) <= len(best):
+                best = [v] + result
+        
+    return best
+
+
+if __name__ == "__main__":
+    assert set(recursive_bestSum(7, [1, 2, 5])) == set([2, 5])
+    assert recursive_bestSum(7, [3, 5]) is None
+    print('Recursive OK!')
+
+
