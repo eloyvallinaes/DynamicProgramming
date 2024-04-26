7c74096c6a58799521885ee15c27c89d5a000df3
evallina <eloyvallina33@gmail.com>
2024-04-26 13:50:01 +0200

    bestSum: memoised

--- bestSum.py
+++ bestSum.py
@@ -35,9 +35,45 @@ def recursive_bestSum(t, arr):
     return best
 
 
+def bestSum(t, arr, memo=None):
+    """
+    Memoised implementation. Time complexity: O(m * t^2)
+        Recursive calls: O(m^t) where len(arr) = m * t
+        List extension: O(k) where len(list) = k
+
+    """
+    # end cases
+    if memo is None:
+        memo = {}
+    
+    if t in memo: return memo[t]
+    if t == 0: return []
+    if t < min(arr): return None
+
+    best = None
+    for v in arr:
+        result = bestSum(t-v, arr, memo)
+        if result is not None:
+            if  best is None or len(result) <= len(best):
+                best = [v] + result  # list extension
+        
+    memo.update({t: best})
+    return best
+
+
 if __name__ == "__main__":
     assert set(recursive_bestSum(7, [1, 2, 5])) == set([2, 5])
     assert recursive_bestSum(7, [3, 5]) is None
     print('Recursive OK!')
+
+    assert set(bestSum(100, [2, 25])) == set([25])
+    print('Memoised OK!')
+
+
+    # timings
+    rec = timeit(recursive_bestSum, 100, [2, 25])
+    mem = timeit(bestSum, 100, [2, 25])
+    print(f"Recursive: {rec:.1f} ms\nMemoised: {mem:.1f} ms")
+
 
 
